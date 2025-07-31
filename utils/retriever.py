2d86d29285943c2f81eab737731ea8cf96e07ef7
niravariya07 <nidhiravariya256@gmail.com>
2025-07-31 22:55:28 +0530

    updated : retriever.py -> importing libraries

--- utils/retriever.py
+++ utils/retriever.py
@@ -2,3 +2,11 @@ import faiss
 import numpy as np
 import pickle
 from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+def load_faiss_index(index_path="faiss_index"):
+    index = faiss.read_index(f"{index_path}/index.faiss")
+    with open(f"{index_path}/chunks.pkl", "rb") as f:
+        chunks = pickle.load(f)
+    return index, chunks
