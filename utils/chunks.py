efba025eb1d1641650303afbf87350b795c40d6a
niravariya07 <nidhiravariya256@gmail.com>
2025-07-31 22:36:24 +0530

    updated : chunks.py -> updated function

---
+++ utils/chunks.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer
+
+def chunk_text(text, model_name="all-MiniLM-L6-v2", max_tokens=512, overlap=50):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokens = tokenizer.encode(text, add_special_tokens=False)
+
+    chunks = []
+    start = 0
+    
+    while start < len(tokens):
+        end = start + max_tokens
+        chunk_tokens = tokens[start:end]
+        chunk_text = tokenizer.decode(chunk_tokens, skip_special_tokens=True)
+        chunks.append(chunk_text)
+        start += max_tokens - overlap
