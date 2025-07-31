91a1ad8cdeba67ae8deccf5af8098a051e389d4e
niravariya07 <nidhiravariya256@gmail.com>
2025-07-31 22:50:21 +0530

    updated : build_index.py -> importing libraries

--- utils/build_idex.py
+++ utils/build_idex.py
@@ -4,4 +4,15 @@ import pickle
 import os
 
 def build_faiss_index(embeddings, chunks, index_path="faiss_index", dim=384):
-    
+    if not os.path.exists(index_path):
+        os.makedirs(index_path)
+
+    index = faiss.IndexFlatL2(dim)
+    index.add(embeddings)
+
+    faiss.write_index(index, os.path.join(index_path, "index.faiss"))
+
+    with open(os.path.join(index_path, "chunks.pkl"), "wb") as f:
+        pickle.dump(chunks, f)
+
+    print(f"FAISS index and chunks saved to '{index_path}'.")
