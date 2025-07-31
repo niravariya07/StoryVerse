4c0c4ec229db5cf00f1d8b6eb5217b8a20d2825c
niravariya07 <nidhiravariya256@gmail.com>
2025-07-31 22:57:09 +0530

    updated : retriever.py -> wrote retrieve_similar_chunks function

--- utils/retriever.py
+++ utils/retriever.py
@@ -12,3 +12,11 @@ def load_faiss_index(index_path="faiss_index"):
     return index, chunks
 
 def retrieve_similar_chunks(query, k=3, index_path="faiss_index"):
+    index, chunks = load_faiss_index(index_path)
+
+    # Embed the query
+    query_embedding = model.encode([query])
+    query_embedding = np.array(query_embedding).astype("float32")
+
+    distances, indices = index.search(query_embedding, k)
+    return [chunks[i] for i in indices[0]]
