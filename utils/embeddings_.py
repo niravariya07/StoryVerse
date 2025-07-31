bbe2be3d8f7b031ee35d38e17b987e15e20f653b
niravariya07 <nidhiravariya256@gmail.com>
2025-07-31 22:42:30 +0530

    updated : embeddings.py -> writing function

--- utils/embeddings_.py
+++ utils/embeddings_.py
@@ -1,3 +1,8 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def embed_text(chunks):
+    embeddings = model.encode(chunks, show_progress_bar=True)
+    return np.array(embeddings)
