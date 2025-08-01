62b47607db4ea44ed4655209401c7c5780d56248
niravariya07 <nidhiravariya256@gmail.com>
2025-08-01 18:17:33 +0530

    updated : app.py -> page config parameters

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from utils.text_extraction_from_pdf import text_extraction_from_text
 
-st.title("Document Text Extraction")
+st.set_page_config(page_title="StoryVerse", layout="centered")
+st.title("StoryVerse - PDF to Story Generator")
 uploaded_file = st.file_uploader("Upload a PDF, DOCX, or TXT file", type=["pdf", "docx", "txt"])
 
 if uploaded_file is not None:
