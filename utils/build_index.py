8c3ade79d91ab83a739d085a879e14b237cec8a3
niravariya07 <nidhiravariya256@gmail.com>
2025-08-01 19:07:14 +0530

    updated : build_index.py

--- utils/build_index.py
+++ utils/build_index.py
@@ -10,9 +10,10 @@ def build_faiss_index(embeddings, chunks, index_path="faiss_index", dim=384):
     index = faiss.IndexFlatL2(dim)
     index.add(embeddings)
 
-    faiss.write_index(index, os.path.join(index_path, "index.faiss"))
+    # faiss.write_index(index, os.path.join(index_path, "index.faiss"))
+    faiss.write_index(index, f"{index_path}/index.faiss")
 
-    with open(os.path.join(index_path, "chunks.pkl"), "wb") as f:
+    with open(f"{index_path}/chunks.pkl", "wb") as f:
         pickle.dump(chunks, f)
 
     print(f"FAISS index and chunks saved to '{index_path}'.")
